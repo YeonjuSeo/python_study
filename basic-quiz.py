5d16ba70180e28ba1b8339b669a4d4bc0538b8bc
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2020-11-30 18:30:49 +0900

    Quiz about Exceiption Handling

--- basic-quiz.py
+++ basic-quiz.py
@@ -127,26 +127,85 @@
 # h3.show_detail()
 
 # solution in lecture
-class House:
-    def __init__(self, location, house_type, deal_type, price, completion_year):
-        self.location=location
-        self.house_type=house_type
-        self.deal_type=deal_type
-        self.price=price
-        self.completion_year=completion_year
+# class House:
+#     def __init__(self, location, house_type, deal_type, price, completion_year):
+#         self.location=location
+#         self.house_type=house_type
+#         self.deal_type=deal_type
+#         self.price=price
+#         self.completion_year=completion_year
 
-    def show_detail(self):
-        print(self.location, self.house_type, self.deal_type, self.price, self.completion_year)
+#     def show_detail(self):
+#         print(self.location, self.house_type, self.deal_type, self.price, self.completion_year)
 
-houses=[]
-house1=House("강남","아파트","매매","10억","2010년")
-house2=House("마포","오피스텔","전세","5억","2007년")
-house3=House("송파","빌라","월세","500/50","2000년")
+# houses=[]
+# house1=House("강남","아파트","매매","10억","2010년")
+# house2=House("마포","오피스텔","전세","5억","2007년")
+# house3=House("송파","빌라","월세","500/50","2000년")
 
-houses.append(house1)
-houses.append(house2)
-houses.append(house3)
+# houses.append(house1)
+# houses.append(house2)
+# houses.append(house3)
 
-print("총 {0}대의 매물이 있습니다.".format(len(houses)))
-for house in houses:
-    house.show_detail()
+# print("총 {0}대의 매물이 있습니다.".format(len(houses)))
+# for house in houses:
+#     house.show_detail()
+
+# Quiz 9
+# # my solution__not solved
+# chicken = 10
+# waiting =1
+# flag=1
+
+# class SoldOutError(Exception):
+#     def __init__(self,msg):
+#         self.msg=msg
+
+#     def __str__(self):
+#         return self.msg
+
+# try:
+#     while(flag==1):
+#         print("[남은 치킨: {0}]" .format(chicken))
+#         order=int(input("치킨을 몇마리 주문하시겠습니까?"))
+#         if(order<1):
+#             raise ValueError
+#         if(order>chicken):
+#             raise SoldOutError("재고가 소진되어 더 이상 주문을 받지 않습니다.")
+#             flag=0
+#         else:
+#             print("[대기번호 {0}] {1} 마리 주문이 완료되었습니다." .format(waiting,order))
+#             waiting+=1
+#             chicken-=order
+# except ValueError:
+#     print("잘못된 값을 입력하였습니다.")
+# except SoldOutError:
+#     print("")
+
+# solution in lecture
+class SoldOutError(Exception):
+    pass
+
+chicken = 10
+waiting = 1
+
+while(True):
+    try:
+        print("[남은 치킨: {0}]" .format(chicken))
+        order=int(input("치킨을 몇마리 주문하시겠습니까?"))
+        if(order>chicken):
+            raise SoldOutError("재고가 소진되어 더 이상 주문을 받지 않습니다.")
+        elif order <=0:
+            raise ValueError
+        else:
+            print("[대기번호 {0}] {1} 마리 주문이 완료되었습니다." .format(waiting,order))
+            waiting+=1
+            chicken-=order
+
+        if(chicken==0):
+            raise SoldOutError
+    except ValueError:
+        print("잘못된 값을 입력하였습니다.")
+    except SoldOutError:
+        print("재고가 소진되어 더 이상 주문을 받지 않습니다.")
+        break
