abf7b6fa56042a88b3cb57e4122f9db668c150a3
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2020-12-02 14:53:06 +0900

    Quiz about Module and Package

--- basic-quiz.py
+++ basic-quiz.py
@@ -183,29 +183,34 @@
 #     print("")
 
 # solution in lecture
-class SoldOutError(Exception):
-    pass
+# class SoldOutError(Exception):
+#     pass
 
-chicken = 10
-waiting = 1
+# chicken = 10
+# waiting = 1
 
-while(True):
-    try:
-        print("[남은 치킨: {0}]" .format(chicken))
-        order=int(input("치킨을 몇마리 주문하시겠습니까?"))
-        if(order>chicken):
-            raise SoldOutError("재고가 소진되어 더 이상 주문을 받지 않습니다.")
-        elif order <=0:
-            raise ValueError
-        else:
-            print("[대기번호 {0}] {1} 마리 주문이 완료되었습니다." .format(waiting,order))
-            waiting+=1
-            chicken-=order
+# while(True):
+#     try:
+#         print("[남은 치킨: {0}]" .format(chicken))
+#         order=int(input("치킨을 몇마리 주문하시겠습니까?"))
+#         if(order>chicken):
+#             raise SoldOutError("재고가 소진되어 더 이상 주문을 받지 않습니다.")
+#         elif order <=0:
+#             raise ValueError
+#         else:
+#             print("[대기번호 {0}] {1} 마리 주문이 완료되었습니다." .format(waiting,order))
+#             waiting+=1
+#             chicken-=order
 
-        if(chicken==0):
-            raise SoldOutError
-    except ValueError:
-        print("잘못된 값을 입력하였습니다.")
-    except SoldOutError:
-        print("재고가 소진되어 더 이상 주문을 받지 않습니다.")
-        break
+#         if(chicken==0):
+#             raise SoldOutError
+#     except ValueError:
+#         print("잘못된 값을 입력하였습니다.")
+#     except SoldOutError:
+#         print("재고가 소진되어 더 이상 주문을 받지 않습니다.")
+#         break
+
+# Quiz 10
+# my solution
+import byme
+byme.sign("YeonjuSeo")
